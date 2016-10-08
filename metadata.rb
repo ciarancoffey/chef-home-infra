a5cae13f7aca4426f3424ed0716344eb155fdeff
Ciarán Coffey <github@ccoffey.ie>
2016-10-08 20:28:01 +0100

    Chef infra setup

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'chef-home-infra'
+maintainer       'Ciarán Coffey'
+maintainer_email 'ciaran@ccoffey.ie'
+license          'all_rights'
+description      'Home infrastructure'
+long_description 'Installs/Configures Home infrastructure'
+version          '0.1.0'
+
