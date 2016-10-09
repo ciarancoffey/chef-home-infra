39852e7197215e739a4927ff655fb27581e7d287
Ciarán Coffey <github@ccoffey.ie>
2016-10-09 14:36:06 +0100

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'ciaran@ccoffey.ie'
 license          'all_rights'
 description      'Home infrastructure'
 long_description 'Installs/Configures Home infrastructure'
-version          '0.1.0'
+version          '0.1.2'
 depends          'users'
 
